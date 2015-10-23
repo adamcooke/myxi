ec882dad7ee2bb8a066eff5792c11c7526e50c77
Adam Cooke <adam@atechmedia.com>
2015-10-23 08:04:17 +0100

    support for multiple subscriptions and unsubs in a single message

--- lib/myxi/default_actions.rb
+++ lib/myxi/default_actions.rb
@@ -1,10 +1,22 @@
 Myxi::Action.add(:Subscribe) do |session, payload|
-  session.subscribe(payload['exchange'], payload['routing_key'])
+  if payload['routing_keys'].is_a?(Array)
+    for key in payload['routing_keys']
+      session.subscribe(payload['exchange'], key)
+    end
+  else
+    session.subscribe(payload['exchange'], payload['routing_key'])
+  end
 end
 
 Myxi::Action.add(:Unsubscribe) do |session, payload|
   if payload['exchange'] && payload['routing_key']
-    session.unsubscribe(payload['exchange'], payload['routing_key'])
+    if payload['routing_keys'].is_a?(Array)
+      for key in payload['routing_keys']
+        session.unsubscribe(payload['exchange'], key)
+      end
+    else
+      session.unsubscribe(payload['exchange'], payload['routing_key'])
+    end
   elsif payload['exchange'] && payload['routing_key'].nil?
     session.unsubscribe_all_for_exchange(payload['exchange'])
   else
